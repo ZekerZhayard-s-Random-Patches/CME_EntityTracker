740ec5434bf80477b76591c37b2735c14f513a80
ZekerZhayard <trees1000@qq.com>
2024-01-28 16:59:15 +0800

    1.0

---
+++ src/main/java/io/github/zekerzhayard/cme_entitytracker/core/ClassTransformer.java
@@ -0,0 +1,39 @@
+package io.github.zekerzhayard.cme_entitytracker.core;
+
+import net.minecraft.launchwrapper.IClassTransformer;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldInsnNode;
+import org.objectweb.asm.tree.InsnNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
+
+public class ClassTransformer implements IClassTransformer {
+    @Override
+    public byte[] transform(String name, String transformedName, byte[] basicClass) {
+        if ("net.minecraft.entity.EntityTracker".equals(transformedName)) {
+            ClassNode cn = new ClassNode();
+            new ClassReader(basicClass).accept(cn, 0);
+            for (MethodNode mn : cn.methods) {
+                if (RemapUtils.checkMethodName(cn.name, mn.name, mn.desc, "<init>") && RemapUtils.checkMethodDesc(mn.desc, "(Lnet/minecraft/world/WorldServer;)V")) {
+                    for (AbstractInsnNode ain : mn.instructions.toArray()) {
+                        if (ain.getOpcode() == Opcodes.PUTFIELD) {
+                            FieldInsnNode min = (FieldInsnNode) ain;
+                            if (RemapUtils.checkClassName(min.owner, "net/minecraft/entity/EntityTracker") && RemapUtils.checkFieldName(min.owner, min.name, min.desc, "field_72793_b") && RemapUtils.checkFieldDesc(min.desc, "Ljava/util/Set;")) {
+                                mn.instructions.insertBefore(ain, new InsnNode(Opcodes.POP));
+                                mn.instructions.insertBefore(ain, new MethodInsnNode(Opcodes.INVOKESTATIC, "java/util/concurrent/ConcurrentHashMap", "newKeySet", "()Ljava/util/concurrent/ConcurrentHashMap$KeySetView;", false));
+                            }
+                        }
+                    }
+                }
+            }
+            ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_MAXS);
+            cn.accept(cw);
+            basicClass = cw.toByteArray();
+        }
+        return basicClass;
+    }
+}
